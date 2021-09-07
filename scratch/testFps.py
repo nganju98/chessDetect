66c3f4c9d2927ce52769705b83b14b2c2e0d683c
Nick Ganju <nick.ganju@gmail.com>
2021-09-06 20:44:28 -0400

    manually focus

--- scratch/testFps.py
+++ scratch/testFps.py
@@ -8,16 +8,21 @@ cap.set(3, 3264)
 cap.set(4, 2448)
 cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
 cfps = cap.get(cv2.CAP_PROP_FPS)
+auto = cap.get(cv2.CAP_PROP_AUTOFOCUS)
+print(f'auto={auto}')
+result = cap.set(cv2.CAP_PROP_FOCUS, 0)
+print(f'result={result}')
 #fps = FPS(5).start()
 while True:
     
     ret, img = cap.read()
-    print(img.shape)
+    #print(img.shape)
     cv2.imshow('img',img)
     k = cv2.waitKey(1) & 0xff
     
     if k == 27:
-        break
+        result = cap.set(cv2.CAP_PROP_FOCUS, 500)
+        print(result)
     #fps.updateAndPrintAndReset()
 cap.release()
 cv2.destroyAllWindows()
