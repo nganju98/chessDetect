ec5ec846ea66073600d8a8b88499887b2904ab3e
Nick Ganju <nick.ganju@gmail.com>
2021-09-09 14:10:05 -0400

    added chess clock

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 
 import os
 import ctypes.util
+from tkinter.constants import N,S,E,W
 
 def set_dll_search_path():
    # Python 3.8 no longer searches for DLLs in PATH, so we have to add
@@ -27,14 +28,79 @@ if os.name == 'nt':
 
 
 from profiler import Profiler
-from tkinter import *  
+
 from PIL import ImageTk,Image  
 import chess
 import chess.svg
 from cairosvg import svg2png
 from io import BytesIO
-from tkinter import ttk
+from tkinter import Canvas, StringVar, Tk, ttk
+import tkinter as tk
 from engine import Engine
+import time
+
+class ChessClock():
+    def __init__(self):
+        self.started = False
+        self.paused = False
+
+    def setAttributes(self, timePerSide,increment):
+        self.timePerSide = timePerSide
+        self.increment = increment
+    
+    def blackButtonPressed(self):
+        if (not self.started):
+            self.started = True
+            self.whiteRemaining = self.timePerSide
+            self.blackRemaning = self.timePerSide
+            self.whiteEndTime = self.whiteRemaining + time.time()
+            self.turn = chess.WHITE
+        else:
+            if (self.turn == chess.BLACK):
+                self.whiteEndTime = self.whiteRemaining + time.time()
+                self.blackRemaning = (self.blackEndTime - time.time()) + self.increment
+                self.turn = chess.WHITE
+        
+            
+
+    def whiteButtonPressed(self):
+        if (self.turn == chess.WHITE):
+            self.blackEndTime = self.blackRemaning + time.time()
+            self.whiteRemaining = (self.whiteEndTime - time.time()) + self.increment
+            self.turn = chess.BLACK
+        
+    def getRemainingTimes(self):
+        if (not self.started):
+            return ChessClock.formatTime(self.timePerSide), ChessClock.formatTime(self.timePerSide)
+        else:
+            if (self.turn == chess.WHITE):
+                whiteTime = self.whiteEndTime - time.time()
+                blackTime = self.blackRemaning
+            if (self.turn == chess.BLACK):
+                whiteTime = self.whiteRemaining
+                blackTime = self.blackEndTime - time.time()
+            return ChessClock.formatTime(whiteTime), ChessClock.formatTime(blackTime)
+
+    def formatTime(time : float):
+        retval = "NOTHING"
+        hours = int(time / 3600)
+        minutes = int((time % 3600) / 60)
+        seconds = int(time) % 60
+        tenths = int(time * 10) % 10
+        
+        if hours > 0:
+            retval = f'{hours:01}' + ':' + f'{minutes:02}' + ':' + f'{seconds:02}'
+        elif minutes > 0:
+            retval = f'{minutes:02}' + ':' + f'{seconds:02}'
+        else:
+           retval=f'{minutes:02}' + ':' + f'{seconds:02}' + '.' + f'{tenths:01}'
+
+        return retval
+
+    
+
+
+
 class ChessGui:
     def __init__(self):
         pass
@@ -42,20 +108,62 @@ class ChessGui:
         
     def start(self):
         self.root = Tk()  
+        style = ttk.Style(self.root)
+        self.root.geometry("800x400")
+        self.root.configure()
+        self.root.rowconfigure(0, weight=0)
+        self.root.columnconfigure(0, weight=1)
+        self.root.columnconfigure(2, weight=1)
+        
+        self.whiteRemaining = StringVar()
+        self.whiteClock = ttk.Label(self.root, font=("Arial", 48), background="white", textvariable=self.whiteRemaining)
+        self.whiteClock.grid(column=0, row=0, sticky=N, pady=10)
+        self.whiteTurn = ttk.Button(self.root, text="White Finished", command=self.whiteButtonPressed)
+        self.whiteTurn.grid(column=0, row=1, sticky=S, pady=10)
+        
+        self.blackRemaining = StringVar()
+        self.blackClock = ttk.Label(self.root, font=("Arial", 48), background="white", textvariable=self.blackRemaining)   
+        self.blackClock.grid(column=2, row=0, sticky=N, pady=10)
+        self.blackTurn = ttk.Button(self.root, text="Black Finished", command=self.blackButtonPressed)
+        self.blackTurn.grid(column=2, row=1, sticky=S, pady=10)
+        
+
         self.canvas = Canvas(self.root, width = 300, height = 300)  
-        self.canvas.pack()  
+        self.canvas.grid(column=1,row=0, sticky=N, pady=10)
+        #self.canvas.pack()  
         self.img = ImageTk.PhotoImage(Image.open("./assets/blank.png"))  
-        self.imageContainer = self.canvas.create_image(0, 0, anchor=NW, image=self.img) 
+        self.imageContainer = self.canvas.create_image(0, 0, anchor=tk.NW, image=self.img) 
         self.text = StringVar()
         self.text.set("Update Text")
-        self.label= ttk.Label(self.root, textvariable=self.text)
-        self.label.pack()
+        self.label= ttk.Label(self.root, font=("Arial"), textvariable=self.text)
+        self.label.grid(row=1, column=1)
+        self.clock = ChessClock()
+        self.clock.setAttributes(300, 5)
+        self.updateClock()
         self.root.mainloop()
         #pass #self.root.mainloop()
 
     # def doMainLoop(self):
         
     #     self.root.mainloop()
+    def whiteButtonPressed(self):
+        self.clock.whiteButtonPressed()
+        self.blackClock.config(background="light yellow")
+        self.whiteClock.config(background="white")
+
+        
+    def blackButtonPressed(self):
+        self.clock.blackButtonPressed()
+        self.whiteClock.config(background="light yellow")
+        self.blackClock.config(background="white")
+
+    def updateClock(self):
+        white, black = self.clock.getRemainingTimes()
+        self.whiteRemaining.set(white)
+        self.blackRemaining.set(black)
+        #self.whiteRemaining, self.blackRemaining = self.clock.getRemainingTimes()
+        self.root.after(20, self.updateClock)
+
 
     def updateChessBoard(self, board : chess.Board):
         self.text.set("Scoring...")
@@ -82,15 +190,34 @@ class ChessGui:
   
 
 if __name__ == "__main__":
+    print(ChessClock.formatTime(300))
+    print(ChessClock.formatTime(3725.5))
+    print(ChessClock.formatTime(12.1))
+    x = 23
+    # clock = ChessClock()
+    # clock.setAttributes(300, 5)
+
+    # print(clock.getRemainingTimes())
+
+    # clock.blackButtonPressed()
+    # time.sleep(2)
+    # print(clock.getRemainingTimes())
+    # clock.whiteButtonPressed()
     
-    # c = ChessGui()
+    # time.sleep(3)
+    # print(clock.getRemainingTimes())
+    
+    # time.sleep(3)
+    # print(clock.getRemainingTimes())
+    c = ChessGui()
+    c.start()
     # threading.Thread(target=c.start, args=()).start()
     # time.sleep(2)
     
-    board = chess.Board().empty()
-    svg = chess.svg.board(board)
-    png = svg2png(bytestring=svg, output_width=300)
-    pil_img = Image.open(BytesIO(png))
+    # board = chess.Board().empty()
+    # svg = chess.svg.board(board)
+    # png = svg2png(bytestring=svg, output_width=300)
+    # pil_img = Image.open(BytesIO(png))
 
         
     #cv_img = cv2.cvtColor(np.array(pil_img), cv2.COLOR_RGBA2BGRA)
